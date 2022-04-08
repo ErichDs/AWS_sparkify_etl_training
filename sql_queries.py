cbb320de468518cdf79a8eb1044aa99cf6ef40c2
erich <erichpdias@hotmail.com>
2022-04-08 17:51:01 -0300

    finished project

--- sql_queries.py
+++ sql_queries.py
@@ -7,142 +7,202 @@ config.read('dwh.cfg')
 
 # DROP TABLES
 
-staging_events_table_drop = "DROP TABLE staging_events"
-staging_songs_table_drop = "DROP TABLE staging_songs"
-songplay_table_drop = "DROP TABLE songplays"
-user_table_drop = "DROP TABLE users"
-song_table_drop = "DROP TABLE songs"
-artist_table_drop = "DROP TABLE artists"
-time_table_drop = "DROP TABLE time"
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 staging_events_table_create= ("""
     CREATE TABLE staging_events (
         artist varchar
         ,auth varchar
-        ,firstName varchar not null
+        ,firstName varchar
         ,gender varchar
         ,itemInSession int
         ,lastName varchar
-        ,length float not null
-        ,level varchar not null
+        ,length float
+        ,level varchar
         ,location varchar
         ,method varchar
         ,page varchar
         ,registration float
         ,sessionId int
-        ,song varchar not null
+        ,song varchar
         ,status int
-        ,ts bigint not null
+        ,ts bigint
         ,userAgent varchar
-        ,userId int not null
+        ,userId int
     )
 """)
 
 staging_songs_table_create = ("""
     CREATE TABLE staging_songs (
         num_songs int
-        ,artist_id varchar not null
+        ,artist_id varchar
         ,artist_latitude float
         ,artist_longitude float
         ,artist_location varchar
         ,artist_name varchar
-        ,song_id varchar not null
-        ,title varchar not null
-        ,duration float not null
+        ,song_id varchar
+        ,title varchar
+        ,duration float
         ,year int
     )
 """)
 
 songplay_table_create = ("""
-    CREATE TABLE songplays (
-        songplay_id IDENTITY(0, 1) PRIMARY KEY,
-        start_time timestamp not null,
-        user_id int not null,
+    CREATE TABLE fct_songplays (
+        songplay_id int identity(1, 1) primary key,
+        start_time timestamp not null sortkey,
+        user_id int not null distkey,
         level varchar,
         song_id varchar,
         artist_id varchar,
         session_id int,
         location varchar,
         user_agent varchar
-    )
+    ) diststyle key
 """)
 
 user_table_create = ("""
-    CREATE TABLE users (
-        user_id int PRIMARY KEY,
+    CREATE TABLE dim_users (
+        user_id int primary key sortkey,
         first_name varchar not null,
         last_name varchar,
         gender varchar,
         level varchar not null
-    )
+    ) diststyle all
 """)
 
 song_table_create = ("""
-    CREATE TABLE songs (
-        song_id varchar PRIMARY KEY,
+    CREATE TABLE dim_songs (
+        song_id varchar primary key sortkey,
         title varchar not null,
-        artist_id varchar,
+        artist_id varchar distkey,
         year int,
         duration float not null
-    )
+    ) diststyle key
 """)
 
 artist_table_create = ("""
-    CREATE TABLE artists (
-        artist_id varchar PRIMARY KEY,
+    CREATE TABLE dim_artists (
+        artist_id varchar primary key sortkey,
         name varchar not null,
         location varchar,
         latitude float,
         longitude float
-    )
+    ) diststyle all
 """)
 
 time_table_create = ("""
-        CREATE TABLE time (
-        start_time timestamp PRIMARY KEY,
-        hour int,
-        day int,
-        week int,
-        month int,
-        year int,
-        weekday int
-    )
+        CREATE TABLE dim_time (
+        start_time timestamp primary key sortkey ,
+        hour smallint,
+        day smallint,
+        week smallint,
+        month smallint,
+        year int distkey,
+        weekday smallint
+    ) diststyle key
 """)
 
 # STAGING TABLES
 
 staging_events_copy = ("""
-""").format()
+    copy staging_events
+    from {bucket}
+    credentials 'aws_iam_role={arn}'
+    region 'us-west-2' format as json {logpath}
+    timeformat as 'epochmillisecs'
+""").format(bucket = config.get('S3', 'LOG_DATA')
+            ,arn = config.get('IAM_ROLE', 'ARN')
+            ,logpath = config.get('S3', 'LOG_JSONPATH'))
 
 staging_songs_copy = ("""
-""").format()
+    copy staging_songs
+    from {bucket}
+    credentials 'aws_iam_role={arn}'
+    region 'us-west-2' format as json 'auto'
+""").format(bucket = config.get('S3', 'SONG_DATA')
+            , arn = config.get('IAM_ROLE', 'ARN'))
 
 # FINAL TABLES
 
 songplay_table_insert = ("""
-    INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
-    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+    INSERT INTO fct_songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+    SELECT DISTINCT
+        TIMESTAMP 'epoch' + (b.ts / 1000) * INTERVAL '1 second' as start_time
+        ,b.userId as user_id
+        ,b.level
+        ,a.song_id
+        ,a.artist_id
+        ,b.sessionId as session_id
+        ,b.location
+        ,b.userAgent as user_agent
+    FROM staging_songs a
+    JOIN staging_events b
+        ON a.title  =   b.song
+        AND a.artist_name    =   b.artist
+    WHERE
+        b.page = 'NextSong'
 """)
 
 user_table_insert = ("""
-    INSERT INTO users (user_id, first_name, last_name, gender, level)
-    VALUES (%s, %s, %s, %s, %s)
+    INSERT INTO dim_users (user_id, first_name, last_name, gender, level)
+    SELECT DISTINCT
+        userId as user_id
+        ,firstName as first_name
+        ,lastName as last_name
+        ,gender
+        ,level
+    FROM staging_events
+    WHERE
+        userId IS NOT NULL
 """)
 
 song_table_insert = ("""
-    INSERT INTO songs (song_id, title, artist_id, year, duration)
-    VALUES (%s, %s, %s, %s, %s)
+    INSERT INTO dim_songs (song_id, title, artist_id, year, duration)
+    SELECT DISTINCT
+        song_id
+        ,title
+        ,artist_id
+        ,year
+        ,duration
+    FROM staging_songs
+    WHERE
+        song_id IS NOT NULL
 """)
 
 artist_table_insert = ("""
-    INSERT INTO artists (artist_id, name, location, latitude, longitude)
-    VALUES (%s, %s, %s, %s, %s)
+    INSERT INTO dim_artists (artist_id, name, location, latitude, longitude)
+    SELECT DISTINCT
+        artist_id
+        ,artist_name
+        ,artist_location
+        ,artist_latitude
+        ,artist_longitude
+    FROM staging_songs a
 """)
 
 time_table_insert = ("""
-    INSERT INTO time (start_time, hour, day, week, month, year, weekday)
-    VALUES (%s, %s, %s, %s, %s, %s, %s)
+    INSERT INTO dim_time (start_time, hour, day, week, month, year, weekday)
+    WITH time_d as (
+        SELECT
+            TIMESTAMP 'epoch' + (b.ts / 1000) * INTERVAL '1 second' as timestamp_col
+        FROM stagin_events)
+    SELECT
+        timestamp_col as start_time
+        ,EXTRACT(hour FROM timestamp_col) as hour
+        ,EXTRACT(day FROM timestamp_col) as day
+        ,EXTRACT(week FROM timestamp_col) as week
+        ,EXTRACT(month FROM timestamp_col) as month
+        ,EXTRACT(year FROM timestamp_col) as year
+        ,EXTRACT(weekday FROM timestamp_col) as weekday
+    from time_d
 """)
 
 # QUERY LISTS
